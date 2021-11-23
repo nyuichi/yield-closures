976bfb76ce7c9e06e0c8396702faa87c7a5b3f6a
Yuichi Nishiwaki <yuichi.nishiwaki@icloud.com>
2021-11-23 17:47:19 +0900

    add README

--- yield-closures-impl/src/lib.rs
+++ yield-closures-impl/src/lib.rs
@@ -1,3 +1,60 @@
+//! # yield-closures
+//!
+//! An implementation of [MCP-49](https://github.com/rust-lang/lang-team/issues/49).
+//!
+//! ```rust
+//! #[test]
+//! fn decode_escape_string() {
+//!     let escaped_text = "Hello,\x20world!\\n";
+//!     let text: String = escaped_text
+//!         .chars()
+//!         .filter_map(co!(|c| {
+//!             loop {
+//!                 if c != '\\' {
+//!                     Not escaped
+//!                     yield Some(c);
+//!                     continue;
+//!                 }
+//!
+//!                 Go past the \
+//!                 yield None;
+//!
+//!                 Unescaped-char
+//!                 match c {
+//!                     Hexadecimal
+//!                     'x' => {
+//!                         yield None; Go past the x
+//!                         let most = c.to_digit(16);
+//!                         yield None; Go past the first digit
+//!                         let least = c.to_digit(16);
+//!                         Yield the decoded char if valid
+//!                         yield (|| char::from_u32(most? << 4 | least?))()
+//!                     }
+//!                     Simple escapes
+//!                     'n' => yield Some('\n'),
+//!                     'r' => yield Some('\r'),
+//!                     't' => yield Some('\t'),
+//!                     '0' => yield Some('\0'),
+//!                     '\\' => yield Some('\\'),
+//!                     Unnecessary escape
+//!                     _ => yield Some(c),
+//!                 }
+//!             }
+//!         }))
+//!         .collect();
+//!     assert_eq!(text, "Hello, world!\n");
+//! }
+//! ```
+//!
+//! For the details of the proposal, see https://lang-team.rust-lang.org/design_notes/general_coroutines.html.
+//!
+//! Differences between this implementation and the proposal are summarized below:
+//!
+//! - This crate offers a macro implementation. It works with the stable Rust.
+//! - No `FnPin` is provided. Yield closures made with this crate use `Box::pin` internally and hence `FnMut`.
+//! - In yield closures, one cannot use `return` expressions.
+//! - The body of a yield closure must be explosive i.e. must not return and typed by the `!` type. Thus it is compatible with both of the two designs of yield closures discussed in the document of MCP-49: poisoning by default or not.
+
 use proc_macro::*;
 use syn::{fold::Fold, ReturnType};
 
@@ -142,7 +199,7 @@ pub fn co(input: TokenStream) -> TokenStream {
     };
     quote::quote!(
         #co_imp_fn(|__arg_rx, __yield_tx| async #capture {
-            let (__arg_rx, __yield_tx) = async move { (__arg_rx, __yield_tx) }.await; // do partial move
+            let (__arg_rx, __yield_tx) = async move { (__arg_rx, __yield_tx) }.await; do partial move
             let (#( mut #inputs ),*);
             ::yield_closures::reassign_args!(__arg_rx, #( #inputs, )*);
             #body
